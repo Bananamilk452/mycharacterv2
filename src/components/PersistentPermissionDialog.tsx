08c02e71cdc893f7b80bd539d0251c250c8dbc7e
Joonsu Yoon <bananamilk452@gmail.com>
2024-12-16 12:09:52 +0900

    PersistentPermissionDialog Message로 변경

--- src/components/PersistentPermissionDialog.tsx
+++ src/components/PersistentPermissionDialog.tsx
@@ -7,11 +7,10 @@ import {
   DialogTitle,
 } from "@/components/ui/Dialog";
 import { Button } from "@/components/ui/Button";
-import { Spinner } from "@/components/ui/Spinner";
 import { usePersistentStorage } from "@/hooks/usePersistentStorage";
 import { useEffect, useState } from "react";
-import { CheckIcon } from "lucide-react";
-import { Note } from "./ui/Note";
+import { Note } from "@/components/ui/Note";
+import { Message } from "@/components/ui/Message";
 
 export function PersistentPermissionDialog() {
   const { ready, isPersistent, persist } = usePersistentStorage();
@@ -47,25 +46,16 @@ export function PersistentPermissionDialog() {
           '시크릿 모드'나 '사생활 보호 모드'에서 마이자캐를 사용하지 마세요.
           브라우저 종료 시 데이터가 유실됩니다.
         </Note>
-        {isPersistRequested && (
-          <div className="flex items-center gap-2">
-            {isPersistent ? (
-              <>
-                <CheckIcon className="size-5 text-green-600" />
-                <span className="text-sm text-green-600">
-                  영구 스토리지 권한이 허용되었습니다.
-                </span>
-              </>
-            ) : (
-              <>
-                <Spinner className="size-5" />
-                <span className="text-sm text-gray-600">
-                  영구 스토리지 권한을 요청 중입니다...
-                </span>
-              </>
-            )}
-          </div>
-        )}
+        {isPersistRequested &&
+          (isPersistent ? (
+            <Message variant="success">
+              영구 스토리지 권한이 허용되었습니다.
+            </Message>
+          ) : (
+            <Message variant="loading">
+              영구 스토리지 권한을 요청 중입니다...
+            </Message>
+          ))}
         <DialogFooter>
           <Button onClick={handlePersist}>영구 스토리지 권한 요청</Button>
           {isPersistent && (
