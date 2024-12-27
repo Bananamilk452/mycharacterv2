d7b14904350cb0c2667b03e0e8689ad1d2755237
Joonsu Yoon <bananamilk452@gmail.com>
2024-12-27 12:17:18 +0900

    CharacterCard 컴포넌트 추가

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,32 @@
+import { Character } from "@/lib/db";
+import { useEffect, useState } from "react";
+
+interface Props {
+  character: Character;
+}
+
+export function CharacterCard({ character }: Props) {
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (character.avatar) {
+      const url = URL.createObjectURL(character.avatar);
+      setAvatarUrl(url);
+
+      return () => {
+        URL.revokeObjectURL(url);
+      };
+    }
+  }, []);
+
+  return (
+    <button className="flex flex-col items-start gap-2 rounded-md border bg-gray-50 p-4 shadow-md">
+      <img
+        className="flex size-32 items-center justify-center rounded-md bg-white object-contain text-sm"
+        src={avatarUrl}
+        alt={character.name}
+      />
+      <h2 className="text-sm font-medium">{character.name}</h2>
+    </button>
+  );
+}
