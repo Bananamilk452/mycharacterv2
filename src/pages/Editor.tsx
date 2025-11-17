a763614b368ed2e564242977aba5dcf119eba1a6
Seoa Yoon <bananamilk452@gmail.com>
2025-11-17 20:08:23 +0900

    Editor 페이지 작동 안하는 버그 수정

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,17 +1,16 @@
 import { AppSidebar } from "@/components/AppSidebar";
 import { CharacterCard } from "@/components/CharacterCard";
+import { Spinner } from "@/components/Spinner";
 import { Dimmer } from "@/components/ui/dimmer";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { Spinner } from "@/components/Spinner";
 import { useCollection } from "@/hooks/useCollection";
 import { useParams } from "crossroad";
 
 function Editor() {
-  const { collectionUuid } = useParams("/editor/:collectionUuid") as {
-    collectionUuid: string;
-  };
-  const { ready, collection, collectionInfo, characters } =
-    useCollection(collectionUuid);
+  const { collectionUuid } = useParams();
+  const { ready, collection, collectionInfo, characters } = useCollection(
+    collectionUuid as string,
+  );
 
   return (
     <main>
@@ -35,8 +34,8 @@ function Editor() {
       ) : (
         <Dimmer>
           <div className="flex flex-col items-center gap-2">
-            <Spinner className="size-6 text-white" />
-            <p className="text-sm text-white">로딩 중...</p>
+            <Spinner className="size-6" />
+            <p className="text-sm">로딩 중...</p>
           </div>
         </Dimmer>
       )}
