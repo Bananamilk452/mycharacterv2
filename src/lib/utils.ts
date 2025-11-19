861b0fb97db27f8fc1c363f519fb726e30ee988f
Seoa Yoon <bananamilk452@gmail.com>
2025-11-19 21:19:56 +0900

    캐릭터 / 콜렉션 업데이트 시 updatedAt 갱신

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,20 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { DB_PREFIX } from "@/constant";
+
+import { connectCollection } from "./db";
+
 import type { ClassValue } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function updateCollectionUpdatedAt(uuid: string) {
+  const collection = await connectCollection(`${DB_PREFIX}${uuid}`);
+
+  await collection.collectionInfo.update(1, {
+    updatedAt: new Date(),
+  });
 }
