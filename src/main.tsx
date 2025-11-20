6c431242a36fdfed688504bbee1083b7745b7d62
Seoa Yoon <bananamilk452@gmail.com>
2025-11-20 19:08:03 +0900

    예전 도메인 사용 시 신규 도메인으로 이동하게

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,15 @@ import "./index.css";
 
 import App from "./App.tsx";
 
+if (location.href === "https://mycharacterv2.vercel.app/") {
+  const go = confirm(
+    "이 도메인은 더 이상 사용되지 않습니다. mycharacter.app으로 이동하시겠습니까?",
+  );
+  if (go) {
+    location.href = "https://mycharacter.app/";
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
